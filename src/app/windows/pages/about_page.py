1f797e542456008cd583d1a6bcce4d7f438cf4c1
toast1599 <toast1599@users.noreply.github.com>
2025-12-27 16:32:45 +0530

    About Page updates

--- src/app/windows/pages/about_page.py
+++ src/app/windows/pages/about_page.py
@@ -14,7 +14,7 @@ class AboutPage(Gtk.Box):
         self.set_margin_start(40)
         self.set_margin_end(40)
 
-        # App icon placeholder (we will replace this later)
+        # App icon
         icon = Gtk.Image.new_from_icon_name("com.toast1599.SimplyToast")
         icon.set_pixel_size(96)
         icon.set_halign(Gtk.Align.CENTER)
@@ -24,15 +24,15 @@ class AboutPage(Gtk.Box):
         title.set_markup("<span size='xx-large'><b>SimplyToast</b></span>")
         title.set_halign(Gtk.Align.CENTER)
 
-        # Version placeholder (replace later with real value)
+        # Version
         version = Gtk.Label(label=f"Version: {VERSION}")
         version.set_halign(Gtk.Align.CENTER)
 
-        # Author / credits
+        # Author
         author = Gtk.Label(label="Created by: toast1599")
         author.set_halign(Gtk.Align.CENTER)
 
-        # Optional description
+        # Description
         desc = Gtk.Label(
             label="A clean and modern utility for managing autostart apps and background tasks."
         )
@@ -40,12 +40,36 @@ class AboutPage(Gtk.Box):
         desc.set_justify(Gtk.Justification.CENTER)
         desc.set_halign(Gtk.Align.CENTER)
 
+        # ---------------- NEW: License notice ----------------
+
+        license_notice = Gtk.Label(
+            label=(
+                "Licensed under the GNU General Public License v3 or later.\n"
+                "This program is provided without any warranty, "
+                "but the author will try to help on a best-effort basis."
+            )
+        )
+        license_notice.set_wrap(True)
+        license_notice.set_justify(Gtk.Justification.CENTER)
+        license_notice.set_halign(Gtk.Align.CENTER)
+        license_notice.get_style_context().add_class("dim-label")
+
+        license_link = Gtk.LinkButton.new_with_label(
+            "https://www.gnu.org/licenses/gpl-3.0.html",
+            "View License"
+        )
+        license_link.set_halign(Gtk.Align.CENTER)
+
+        # ----------------------------------------------------
+
         # Append widgets
         self.append(icon)
         self.append(title)
         self.append(version)
         self.append(author)
         self.append(desc)
+        self.append(license_notice)
+        self.append(license_link)
 
     def refresh(self):
-        pass  # Nothing to refresh right now
+        pass
